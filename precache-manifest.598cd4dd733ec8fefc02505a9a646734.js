0f17f5cf8fec19b76202771048ba3014c869b347
dadayada <nasushnyi@gmail.com>
2019-10-16 23:27:05 +0300

    Updates

---
+++ precache-manifest.598cd4dd733ec8fefc02505a9a646734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a2c6f2adb0437599dfa86cc986dc33",
+    "url": "/taxi-way/index.html"
+  },
+  {
+    "revision": "01e3b78b0a7f66471c87",
+    "url": "/taxi-way/static/css/main.6f694fae.chunk.css"
+  },
+  {
+    "revision": "3bf74db8dc59dea37b9a",
+    "url": "/taxi-way/static/js/2.457d710d.chunk.js"
+  },
+  {
+    "revision": "01e3b78b0a7f66471c87",
+    "url": "/taxi-way/static/js/main.b6faefa6.chunk.js"
+  },
+  {
+    "revision": "21e25e1034c3eed057b9",
+    "url": "/taxi-way/static/js/runtime~main.ad0ad32f.js"
+  }
+]);
